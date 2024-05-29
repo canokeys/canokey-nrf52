dac4357f7a1585b6108fc5dfaadb226782bc0155
z4yx <z4yx@users.noreply.github.com>
2024-05-29 23:41:51 +0800

    porting touch detection from STM32

--- src/device.c
+++ src/device.c
@@ -11,6 +11,11 @@
 
 #include <boards.h>
 
+const uint32_t TOUCH_GAP_TIME = 800; /* Gap period (in ms) between two consecutive touch events */
+const uint32_t MIN_LONG_TOUCH_TIME = 500;
+const uint32_t MIN_TOUCH_TIME = 20;
+
+
 uint32_t device_get_tick() {
     return system_ticks;
 }
@@ -31,25 +36,68 @@ void led_off() {
     bsp_board_led_off(BSP_BOARD_LED_0);
 }
 
+inline static bool btn_down() {
+    return bsp_board_button_state_get(BSP_BOARD_BUTTON_0);
+}
+
 static void (*tim_callback)(void);
 
 // every 10ms
 void timer_touch_handler(nrf_timer_event_t event_type, void *p_context) {
-    static uint32_t touch_timeout = 1;
-    device_update_led();
-    if(has_touch) {
-        if(bsp_board_button_state_get(BSP_BOARD_BUTTON_0)) {
-            uint32_t tick = device_get_tick();
-            if(tick < touch_timeout + 1000) {
-            } else {
-                set_touch_result(TOUCH_SHORT);
-                touch_timeout = tick + 200;
-            }
-        } else if(device_get_tick() > touch_timeout)
-            set_touch_result(TOUCH_NO);
-    } else {
-        set_touch_result(TOUCH_SHORT);
+  enum {
+    TOUCH_STATE_IDLE,
+    TOUCH_STATE_DOWN,
+    TOUCH_STATE_ASSERT,
+    TOUCH_STATE_DEASSERT,
+  };
+  static uint32_t event_tick, fsm = TOUCH_STATE_IDLE;
+  uint32_t tick = device_get_tick();
+  switch (fsm)
+  {
+  case TOUCH_STATE_IDLE:
+#ifdef DEBUG_OUTPUT
+    // Emulate touch events with UART input
+    // if (LL_USART_IsActiveFlag_RXNE(DBG_UART.Instance)) {
+    //   int data = LL_USART_ReceiveData8(DBG_UART.Instance);
+    //   DBG_MSG("UART: %x\n", data);
+    //   if ('T' == data || 'L' == data) {
+    //     set_touch_result('T' == data ? TOUCH_SHORT : TOUCH_LONG);
+    //     fsm = TOUCH_STATE_ASSERT;
+    //     event_tick = tick;
+    //   }
+    // }
+#endif
+    if(has_touch && btn_down()) {
+      fsm = TOUCH_STATE_DOWN;
+      event_tick = tick;
     }
+    break;
+  case TOUCH_STATE_DOWN:
+    if(!btn_down() || tick - event_tick > MIN_LONG_TOUCH_TIME) {
+      if (tick - event_tick > MIN_TOUCH_TIME) {
+        set_touch_result(tick - event_tick > MIN_LONG_TOUCH_TIME ? TOUCH_LONG : TOUCH_SHORT);
+        fsm = TOUCH_STATE_ASSERT;
+        event_tick = tick;
+      } else
+        fsm = TOUCH_STATE_IDLE;
+    }
+    break;
+  case TOUCH_STATE_ASSERT:
+    if (tick - event_tick > TOUCH_GAP_TIME) {
+      set_touch_result(TOUCH_NO);
+      fsm = TOUCH_STATE_DEASSERT;
+      DBG_MSG("touch deasserted\r\n");
+    }
+    break;
+  case TOUCH_STATE_DEASSERT:
+    if(!btn_down()) {
+      fsm = TOUCH_STATE_IDLE;
+    }
+    break;
+  default:
+    break;
+  }
+  device_update_led();
 }
 
 void device_set_timeout(void (*callback)(void), uint16_t timeout) {
